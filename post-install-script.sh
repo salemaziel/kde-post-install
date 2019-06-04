6ffa3dd76c8c471b173d3d798a880c891856c440
salemaziel <51006863+salemaziel@users.noreply.github.com>
2019-06-04 01:21:00 -0700

    Add files via upload

--- post-install-script.sh
+++ post-install-script.sh
@@ -19,7 +19,7 @@ else
     echo " Cool, continuing "
 fi
 
-sudo apt purge exim4 avahi-autoipd avahi-daemon cups cups-browsed unattended-upgrades cryptsetup-initramfs kate kate5-data -y
+sudo apt purge exim4 avahi-autoipd avahi-daemon cups cups-browsed unattended-upgrades cryptsetup-initramfs -y
 
 
 ## install repos and their keys and their programs
@@ -55,62 +55,62 @@ cd ;
 
 echo " ****** installing protonvpn ******* "
 wget "https://github.com/ProtonVPN/protonvpn-cli/raw/master/protonvpn-cli.sh" -O "protonvpn-cli.sh" && sudo bash protonvpn-cli.sh --install
-echo " *** protonvpn openvpn username: code_vpn *** "
-sleep 2
-echo " *** text salem or login to protonvpn account in browser for protonvpn/openvpn password *** "
-sleep 3
-echo " *** choose n for custom dns, y for downgrading openvpn privileges, and choose 4) visionary plan when it tells you to choose which plan you have. The code doesnt actually check which plan you have lol *** "
-sleep 3
-./protonvpn-cli.sh --init
+#echo " *** protonvpn openvpn username: code_vpn *** "
+#sleep 2
+#echo " *** text salem or login to protonvpn account in browser for protonvpn/openvpn password *** #"
+#sleep 3
+#echo " *** choose n for custom dns, y for downgrading openvpn privileges, and choose 4) visionary #plan when it tells you to choose which plan you have. The code doesnt actually check which plan #you have lol *** "
+#sleep 3
+#./protonvpn-cli.sh --init
 
-echo " *** making a few custom aliases for using protonvpn; adding sudo to alias, so you dont have to type it out, just type pvpn and the letter string argument *** "
-mkalias new pvpn "sudo pvpn"
+#echo " *** making a few custom aliases for using protonvpn; adding sudo to alias, so you dont have #to type it out, just type pvpn and the letter string argument *** "
+#mkalias new pvpn "sudo pvpn"
 
-echo " *** new custom alias pvpn-rcf = reconnect to fastest. Disconnects you from proton, then reconnects to current fastest available" 
-mkalias new pvpn-rcf "pvpn -d && pvpn -f"
-sleep 2
+#echo " *** new custom alias pvpn-rcf = reconnect to fastest. Disconnects you from proton, then reconnects to current fastest available" 
+#mkalias new pvpn-rcf "pvpn -d && pvpn -f"
+#sleep 2
 
-echo " *** new custom alias pvpn-rcf = reconnect to fastest. Disconnects you from proton, then reconnects to current fastest available" 
-mkalias new pvpn-m "pvpn -d && pvpn -m"
-sleep 2
+#echo " *** new custom alias pvpn-rcf = reconnect to fastest. Disconnects you from proton, then #reconnects to current fastest available" 
+#mkalias new pvpn-m "pvpn -d && pvpn -m"
+#sleep 2
 
-echo " ** making a text file in your Home folder with all protonvpn commands including the custom ones just made ** "
-touch ~/protonvpn-commands.txt
+#echo " ** making a text file in your Home folder with all protonvpn commands including the custom #ones just made ** "
+#touch ~/protonvpn-commands.txt
 
-echo "
-Usage: protonvpn-cli.sh [option]
-OR
-pvpn [option]
+#echo "
+#Usage: protonvpn-cli.sh [option]
+#OR
+#pvpn [option]
 
-custom:
-pvpn = sudo pvpn
-pvpn-rcf = disconnect from current protonvpn server and reconnect to fastest
-pvpn-m = disconnect from current protonvpn server, and bring up a menu to select your server 
+#custom:
+#pvpn = sudo pvpn
+#pvpn-rcf = disconnect from current protonvpn server and reconnect to fastest
+#pvpn-m = disconnect from current protonvpn server, and bring up a menu to select your server 
 
 
-Standard Options:
-   --init                              Initialize ProtonVPN profile on the machine.
-   -c, --connect                       Select and connect to a ProtonVPN server.
-   -c [server-name] [protocol]         Connect to a ProtonVPN server by name.
-   -m, --menu                          Select and connect to a ProtonVPN server from a menu.
-   -r, --random-connect                Connect to a random ProtonVPN server.
-   -l, --last-connect                  Connect to the previously used ProtonVPN server.
-   -f, --fastest-connect               Connect to the fastest available ProtonVPN server.
-   -p2p, --p2p-connect                 Connect to the fastest available P2P ProtonVPN server.
-   -tor, --tor-connect                 Connect to the fastest available ProtonVPN TOR server.
-   -sc, --secure-core-connect          Connect to the fastest available ProtonVPN SecureCore server.
-   -cc, --country-connect              Select and connect to a ProtonVPN server by country.
-   -cc [country-name] [protocol]       Connect to the fastest available server in a specific country.
-   -d, --disconnect                    Disconnect the current session.
-   --reconnect                         Reconnect to the current ProtonVPN server.
-   --ip                                Print the current public IP address.
-   --status                            Print connection status.
-   --update                            Update protonvpn-cli.
-   --install                           Install protonvpn-cli.
-   --uninstall                         Uninstall protonvpn-cli.
-   -v, --version                       Display version.
-   -h, --help                          Show this help message. " >> ~/protonvpn-commands.txt
-
+#Standard Options:
+#   --init                              Initialize ProtonVPN profile on the machine.
+#   -c, --connect                       Select and connect to a ProtonVPN server.
+#   -c [server-name] [protocol]         Connect to a ProtonVPN server by name.
+#   -m, --menu                          Select and connect to a ProtonVPN server from a menu.
+#   -r, --random-connect                Connect to a random ProtonVPN server.
+#   -l, --last-connect                  Connect to the previously used ProtonVPN server.
+#   -f, --fastest-connect               Connect to the fastest available ProtonVPN server.
+#   -p2p, --p2p-connect                 Connect to the fastest available P2P ProtonVPN server.
+#   -tor, --tor-connect                 Connect to the fastest available ProtonVPN TOR server.
+#   -sc, --secure-core-connect          Connect to the fastest available ProtonVPN SecureCore #server.
+#   -cc, --country-connect              Select and connect to a ProtonVPN server by country.
+#   -cc [country-name] [protocol]       Connect to the fastest available server in a specific #country.
+#   -d, --disconnect                    Disconnect the current session.
+#   --reconnect                         Reconnect to the current ProtonVPN server.
+#   --ip                                Print the current public IP address.
+#   --status                            Print connection status.
+#   --update                            Update protonvpn-cli.
+#   --install                           Install protonvpn-cli.
+#   --uninstall                         Uninstall protonvpn-cli.
+#   -v, --version                       Display version.
+#   -h, --help                          Show this help message. " >> ~/protonvpn-commands.txt
+#
    
    
 echo " *** installing signal messenger for desktop *** "
@@ -134,12 +134,60 @@ fi
 
 
 echo " *** installing caprine:fb messenger for linux *** "
+cd ~/Documents/Zips
 curl -s https://api.github.com/repos/sindresorhus/caprine/releases/latest \
 | grep "browser_download_url.*deb" \
 | cut -d : -f 2,3 \
 | tr -d \" \
 | tail -1 | wget -O caprine.deb -qi -
 sudo gdebi -n caprine.deb
+
+
+echo " *** installing teamviewer for linux *** "
+cd ~/Documents/Zips
+wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+sudo gdebi -n teamviewer_amd64.deb
+
+
+echo " *** installing pulse-sms *** "
+cd ~/Documents/Zips ;
+wget https://github.com/klinker-apps/messenger-desktop/releases/download/v3.4.3/pulse-sms-3.4.3-amd64.deb
+sudo gdebi -n pulse-sms*.deb
+
+
+echo " *** installing google chrome *** "
+cd ~/Documents/Zips ;
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo gdebi -n google-chrome-stable*.deb
+
+
+echo " *** installing protonmail desktop:unofficial *** "
+cd ~/Documents/Zips ;
+wget https://github.com/protonmail-desktop/application/releases/download/v1.0.2/protonmail-desktop-unofficial_1.0.2_amd64.deb
+sudo gdebi -n protonmail-desktop-unofficial*.deb
+
+
+echo " *** installing slack *** "
+cd ~/Documents/Zips ;
+wget https://downloads.slack-edge.com/linux_releases/slack-desktop-3.4.2-amd64.deb
+sudo gdebi -n slack-desktop*.deb
+
+
+echo " *** installing virtualbox *** "
+cd ~/Documents/Zips ;
+wget https://download.virtualbox.org/virtualbox/6.0.8/virtualbox-6.0_6.0.8-130520~Ubuntu~bionic_amd64.deb
+wget https://download.virtualbox.org/virtualbox/6.0.8/Oracle_VM_VirtualBox_Extension_Pack-6.0.8.vbox-extpack
+sudo gdebi -n virtualbox*.deb
+
+echo " *** installing vivaldi *** "
+cd ~/Documents/Zips ;
+wget https://downloads.vivaldi.com/stable/vivaldi-stable_2.5.1525.48-1_amd64.deb
+sudo gdebi -n vivaldi-stable*.deb
+
+echo " *** installing zoom *** "
+cd ~/Documents/Zips ;
+wget https://zoom.us/client/latest/zoom_amd64.deb
+sudo gdebi -n zoom_amd64.deb
 
 
 echo " *** Installing from Zips folder ** "
@@ -153,17 +201,16 @@ sudo gdebi -n discord-0.0.9.deb
 sudo gdebi -n encryptr_2.0.0-1_amd64.deb
 sudo gdebi -n firejail-apparmor_0.9.56-LTS_1_amd64.deb
 sudo gdebi -n firetools_0.9.52_1_amd64.deb
-sudo gdebi -n google-chrome-stable_current_amd64.deb
-sudo gdebi -n protonmail-desktop_0.5.9_amd64.deb
-sudo gdebi -n pulse-sms-3.1.4-amd64.deb
-sudo gdebi -n slack-desktop-3.3.7-amd64.deb
+#sudo gdebi -n google-chrome-stable_current_amd64.deb
+#sudo gdebi -n protonmail-desktop_0.5.9_amd64.deb
+#sudo gdebi -n pulse-sms-3.1.4-amd64.deb
+#sudo gdebi -n slack-desktop-3.3.7-amd64.deb
 sudo gdebi -n stacer_1.0.9_amd64.deb
-sudo gdebi -n teamviewer_13.1.8286_amd64.deb
 sudo gdebi -n upwork_5.1.0.647_amd64.deb
-sudo gdebi -n virtualbox-6.0_6.0.6-130049~Ubuntu~bionic_amd64.deb
-sudo gdebi -n vivaldi-stable_2.3.1440.60-1_amd64.deb
+#sudo gdebi -n virtualbox-6.0_6.0.6-130049~Ubuntu~bionic_amd64.deb
+#sudo gdebi -n vivaldi-stable_2.3.1440.60-1_amd64.deb
 sudo gdebi -n youtube-music-desktop_0.2.1_amd64.deb
-sudo gdebi -n zoom_amd64.deb
+#sudo gdebi -n zoom_amd64.deb
 
 sudo apt update
 sudo apt -y full-upgrade
@@ -194,7 +241,7 @@ sudo chmod +x /opt/basecamp-linux-x64/libnode.so
 sudo chmod +x /opt/basecamp-linux-x64/libffmpeg.so
 sudo ln -s /opt/basecamp-linux-x64/basecamp /usr/local/bin/basecamp
 
-echo " ** Basecamp is installed. Find it in the application menu or run it from the terminal with the command: basecamp ** "p
+#echo " ** Basecamp is installed. Find it in the application menu or run it from the terminal with #the command: basecamp ** "p
 
 
 
@@ -210,6 +257,7 @@ gcloud components update
 ## install nodeJS 10
 echo " ** Installing NodeJS 10 and npm node package manager ** "
 cd ;
+sudo apt install gcc g++ make -y
 mkdir ~/.npm-global
 curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
 sudo apt update
