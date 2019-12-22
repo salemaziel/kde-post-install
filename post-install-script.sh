24e2826973b2ef68371cb055cc65f5ecd58146eb
salem <salem@laCosa>
2019-12-21 22:43:37 -0800

    reset order of dialog option actions

--- post-install-script.sh
+++ post-install-script.sh
@@ -408,6 +408,31 @@ os_options=(1 "Ubuntu: Gnome" off
          6 "GalliumOS: KDE" off)
 os_choices=$("${command[@]}" "${os_options[@]}" 2>&1 >/dev/tty)
 clear
+for os_choice in $os_choices
+    do
+    case $os_choice in
+        1)
+            gnome_install
+            ;;
+        2)
+            kubuntu_install
+            ;;
+        3)
+            lubuntu_install
+            ;;
+        4)
+            xubuntu_install
+            ;;
+        5)
+            echo_note "Gallium XFCE not finished. Run again and pick something else"
+            exit 1
+            ;;
+        6)
+            echo_note "Gallium KDE not finished. Run again and pick something else"
+            exit 1
+            ;;
+    esac
+done
 
 cmd=(dialog --separate-output --checklist "Select Extra Apps To Install. Default is to Install All. Navigate with Up/Down Arrows. Select/Unselect with Spacebar. Hit Enter key When Finished To Continue. ESC key/Cancel exits and continues without installing any options" 22 176 16)
 options=(1 "Etcher: Live USB creator" on
@@ -442,16 +467,6 @@ options=(1 "Etcher: Live USB creator" on
          30 "Visual Studio Code: Advanced Text Editor" on)
 choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
 clear
-
-echo "                          "
-echo "                          "
-echo "                          "
-echo_info "Running updates first"
-echo "                          "
-
-sudo apt update
-
-
 for os_choice in $os_choices
 do
     case $os_choice in
@@ -477,6 +492,16 @@ do
             ;;
     esac
 done
+echo "                          "
+echo "                          "
+echo "                          "
+echo_info "Running updates first"
+echo "                          "
+
+sudo apt update
+
+
+
 
 
 
